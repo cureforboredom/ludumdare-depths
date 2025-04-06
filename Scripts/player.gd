810b89f2a435148f3897bad53d591c18072d1cb3
cure <git@cureforboredom.uk>
2025-04-06 17:41:32 -0600

    progress

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -60,113 +60,123 @@ func _physics_process(delta: float) -> void:
       else:
         momentum = {"speed": 0, "dir": 0, "jump": false, "decel": 0}
 
-  # Handle jump.
-  if (RLW.is_colliding() or RRW.is_colliding()):
-    wall_timer = WALL_TIME
-  else:
-    if wall_timer > 0:
-      wall_timer -= delta
-    
-  if Input.is_action_just_pressed("Jump") and accept_input:
-    jump_buffer_timer = JUMP_BUFFER
-  elif jump_timer > 0:
-    if Input.is_action_pressed("Jump") and accept_input:
-      velocity.y += JUMP_VELOCITY / 25
-      jump_timer -= delta
+  if accept_input:
+    # Handle jump.
+    if (RLW.is_colliding() or RRW.is_colliding()):
+      wall_timer = WALL_TIME
     else:
-      jump_timer = 0
-  
-  if jump_buffer_timer > 0:
-    if is_on_floor() or coyote_timer > 0:
-      #snap around ledges
-      if RLO.is_colliding() and !RLI.is_colliding() \
-        and !RRI.is_colliding() and !RRO.is_colliding():
-          position.x += 5
-          slip = -15
-      if RRO.is_colliding() and !RRI.is_colliding() \
-        and !RLI.is_colliding() and !RLO.is_colliding():
-          position.x -= 5
-          slip = 15
-
-      velocity.y = JUMP_VELOCITY
-      jump_buffer_timer = 0
-      jump_timer = JUMP_TIME
-
-    elif wall_timer > 0:
-      velocity.y = JUMP_VELOCITY * 0.8
-      jump_timer = JUMP_TIME
-      wall_jump_x = (
-        (int(RLW.is_colliding()) * 1) \
-        + (int(RRW.is_colliding()) * -1)
-        ) * 150
-      wall_timer = 0
-      jump_buffer_timer = 0
-      double_jump = true
-
-    elif double_jump:
-      velocity.y = JUMP_VELOCITY * 0.9
-      jump_timer = JUMP_TIME * 0.5
-      double_jump = false
-      jump_buffer_timer = 0
-
-    else:
-      jump_buffer_timer -= delta
+      if wall_timer > 0:
+        wall_timer -= delta
       
+    if Input.is_action_just_pressed("Jump") and accept_input:
+      jump_buffer_timer = JUMP_BUFFER
+    elif jump_timer > 0:
+      if Input.is_action_pressed("Jump") and accept_input:
+        velocity.y += JUMP_VELOCITY / 25
+        jump_timer -= delta
+      else:
+        jump_timer = 0
+    
+    if jump_buffer_timer > 0:
+      if is_on_floor() or coyote_timer > 0:
+        #snap around ledges
+        if RLO.is_colliding() and !RLI.is_colliding() \
+          and !RRI.is_colliding() and !RRO.is_colliding():
+            position.x += 5
+            slip = -15
+        if RRO.is_colliding() and !RRI.is_colliding() \
+          and !RLI.is_colliding() and !RLO.is_colliding():
+            position.x -= 5
+            slip = 15
 
-  if Input.is_action_just_pressed("Left") and accept_input:
-    velocity.x = INIT_SPEED * -1
-  elif Input.is_action_just_pressed("Right") and accept_input:
-    velocity.x = INIT_SPEED
-  else:
-    if accept_input:
-      direction = Input.get_axis("Left", "Right")
-      if wall_jump_x != 0:
-        direction += wall_jump_x / abs(wall_jump_x) / 2
-        wall_jump_x -= delta * direction * 10
-      if direction:
-        if momentum["speed"] > 0 and \
-        (direction/abs(direction)) == momentum["dir"] and \
-        !momentum["jump"] or (momentum["jump"] and (jump_timer > 0)):
+        velocity.y = JUMP_VELOCITY
+        jump_buffer_timer = 0
+        jump_timer = JUMP_TIME
+
+      elif wall_timer > 0:
+        velocity.y = JUMP_VELOCITY * 0.8
+        jump_timer = JUMP_TIME
+        wall_jump_x = (
+          (int(RLW.is_colliding()) * 1) \
+          + (int(RRW.is_colliding()) * -1)
+          ) * 150
+        wall_timer = 0
+        jump_buffer_timer = 0
+        double_jump = true
+
+      elif double_jump:
+        velocity.y = JUMP_VELOCITY * 0.9
+        jump_timer = JUMP_TIME * 0.5
+        double_jump = false
+        jump_buffer_timer = 0
+
+      else:
+        jump_buffer_timer -= delta
+        
+    # make movement more responsive by immediately changing horizontal velocity when an action is pressed
+    if Input.is_action_just_pressed("Left") and accept_input:
+      velocity.x = INIT_SPEED * -1
+    elif Input.is_action_just_pressed("Right") and accept_input:
+      velocity.x = INIT_SPEED
+    # apply acceleration after first frame of horizontal input
+    else:
+        direction = Input.get_axis("Left", "Right")
+
+        if direction:
+          var added_speed = slip
+
+          # check if momentum should be applied
+          if momentum["speed"] > 0 and (
+            direction/abs(direction)) == momentum["dir"] and \
+          !momentum["jump"] or (momentum["jump"] and (jump_timer > 0)
+          ):
+            # apply momentum and lower it
+            added_speed += momentum["speed"] * momentum["dir"]
+            momentum["speed"] -= momentum["decel"] * delta
+          else:
+            momentum = {
+              "speed": 0, "dir": 0, "jump": false, "decel": 0
+              }
+            
+          if jump_timer > 0:
+            added_speed += MAX_SPEED * 0.5
+
           velocity.x = move_toward(
-            velocity.x, MAX_SPEED * direction * (int(jump_timer > 0) * 1.5) \
-            + momentum["speed"] * momentum["dir"] + slip,
+            velocity.x, (MAX_SPEED + added_speed) * direction,
             ACCEL * delta
           )
-          momentum["speed"] -= momentum["decel"] * delta
         else:
-          momentum = {"speed": 0, "dir": 0, "jump": false, "decel": 0}
-          velocity.x = move_toward(
-            velocity.x, MAX_SPEED * direction * (int(jump_timer > 0) * 0.5 + 1) \
-            + slip, ACCEL * delta
-            )
+          velocity.x = move_toward(velocity.x, slip, ACCEL * delta)
+    
+    if wall_jump_x != 0:
+      velocity.x = wall_jump_x
+      wall_jump_x = 0
+    
+    if is_on_floor():
+      if abs(slip) >= 5:
+        slip += (slip / 5) * -1
       else:
-        velocity.x = move_toward(velocity.x, 0 + slip, ACCEL * delta)
-  
-  if wall_jump_x != 0:
-    velocity.x = wall_jump_x
-    wall_jump_x = 0
-  
-  if is_on_floor():
-    if abs(slip) >= 5:
-      slip += (slip / 5) * -1
+        slip = 0
+      coyote_timer = COYOTE_TIME
+      double_jump = true
     else:
-      slip = 0
-    coyote_timer = COYOTE_TIME
-    double_jump = true
-  else:
-    if coyote_timer > 0:
-      coyote_timer -= delta
+      if coyote_timer > 0:
+        coyote_timer -= delta
 
+  # animations
   if accept_input:
     direction = Input.get_axis("Left", "Right")
     if direction:
       animplayer.play("Run")
       sprite.flip_h = direction < 0
+      
   if falling and is_on_floor():
     falling = false
     animplayer.play("Landing")
+
   if velocity.y < 0:
     animplayer.play("Jump up")
+
   if velocity.y > 0:
     animplayer.play("Jump down")
     
